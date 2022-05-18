3f7992b2a264877eb2b25f683ecc00f5a4212573
BaslealAkliluNeymar <baslealaklilu@gmail.com>
2022-05-18 11:37:22 -0700

    REST

--- print_nums.c
+++ print_nums.c
@@ -1,80 +1,83 @@
 #include "main.h"
 
-/*
- * print_int - prints integer
- * @l: va_list arguments of printf
- * @f: pointer to the struct flag
- * Return: list of charater to be printed
+/**
+ * print_int - prints an integer
+ * @l: va_list of arguments from _printf
+ * @f: pointer to the struct flags determining
+ * if a flag is passed to _printf
+ * Return: number of char printed
  */
 int print_int(va_list l, flags_t *f)
 {
-  int a = va_arg(l, int);
-  int b = count_digit(a);
+int n = va_arg(l, int);
+int res = count_digit(n);
 
-  if (f->space == 1 && f->plus == 0 && a >= 0)
-    b += _putchar(' ');
-  if (f->plus == 1 && a >= 0)
-    b += _putchar('+');
-  if (a <= 0)
-    b++;
-  print_number(a);
-  return (b);
+if (f->space == 1 && f->plus == 0 && n >= 0)
+res += _putchar(' ');
+if (f->plus == 1 && n >= 0)
+res += _putchar('+');
+if (n <= 0)
+res++;
+print_number(n);
+return (res);
 }
 
-/*
- * print_unsigned - prints  unsigned integer
- * @l: va_list arguments of print f
- * @f: pointer to the struct flag
- * Return: list of char to be printed
+/**
+ * print_unsigned - prints an unsigned integer
+ * @l: va_list of arguments from _printf
+ * @f: pointer to the struct flags determining
+ * if a flag is passed to _printf
+ * Return: number of char printed
  */
 int print_unsigned(va_list l, flags_t *f)
 {
-  unsigned int c = va_arg(l, unsigned int);
-  char *s = convert(c, 10, 0);
-
-  (void)f;
-  return (_puts(s));
+unsigned int u = va_arg(l, unsigned int);
+char *str = convert(u, 10, 0);
+(void)f;
+return (_puts(str));
 }
 
-/*
- * print_number - function that loops through intigers and print all digits
+/**
+ * print_number - helper function that loops through
+ * an integer and prints all its digits
  * @n: integer to be printed
  */
 void print_number(int n)
 {
-  unsigned int n1;
+unsigned int n1;
 
-  if (n < 0)
-  {
-    _putchar('-');
-    n1 = -n;
-  }
-  else
-    n1 = n;
+if (n < 0)
+{
+_putchar('-');
+n1 = -n;
+}
+else
+n1 = n;
 
-  if (n1 / 10)
-    print_number(n1 / 10);
-  _putchar((n1 % 10) + '0');
+if (n1 / 10)
+print_number(n1 / 10);
+_putchar((n1 % 10) + '0');
 }
 
-/*
- * count_digit - returns no of digits
- * @i: integer to determine
+/**
+ * count_digit - returns the number of digits in an integer
+ * for _printf
+ * @i: integer to evaluate
  * Return: number of digits
  */
 int count_digit(int i)
 {
-  unsigned int d = 0;
-  unsigned int k;
+unsigned int d = 0;
+unsigned int u;
 
-  if (i < 0)
-    k = i * -1;
-  else
-    k = i;
-  while (k != 0)
-  {
-    k /= 10;
-    d++;
-  }
-  return (d);
+if (i < 0)
+u = i * -1;
+else
+u = i;
+while (u != 0)
+{
+u /= 10;
+d++;
+}
+return (d);
 }
